2d387f7609369b73bc47b92858cb3eada7fb15dc
Jonas Hægdahl <jonas.haegd@gmail.com>
2021-04-27 23:28:35 +0200

    Add git command

---
+++ Commands/Info.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeckoV2.Commands
+{
+    public class Info : ModuleBase<SocketCommandContext>
+    {
+        [Command("Git")]
+        public async Task GetGit() => await ReplyAsync("https://github.com/ScarredSceptile/TreeckoDiscordBot");
+    }
+}
